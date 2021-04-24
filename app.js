16b070d4bbdbcc5e8da1e724b403c7da2918c2b4
https://github.com/Snva2031/CI_60.git <hongsontb2001@gmail.com>
2021-04-24 20:45:48 +0700

    hi

---
+++ app.js
@@ -0,0 +1,287 @@
+const data = [
+  {
+      "id": 1,
+      "name": "Yehudit Toffler",
+      "gender": "Male",
+      "country": "Cuba",
+      "phoneNumber": "+53 393 419 6210",
+      "favorites": [
+          "film",
+          "drink"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/5fa2dd/ffffff"
+  },
+  {
+      "id": 2,
+      "name": "Lamar Lawles",
+      "gender": "Female",
+      "country": "Vietnam",
+      "phoneNumber": "+84 732 412 4632",
+      "favorites": [
+          "drink",
+          "sport"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/cc0000/ffffff"
+  },
+  {
+      "id": 3,
+      "name": "Wynne Skingle",
+      "gender": "Female",
+      "country": "France",
+      "phoneNumber": "+33 944 315 4636",
+      "favorites": [
+          "sport",
+          "music",
+          "film",
+          "food",
+          "drink"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/dddddd/000000"
+  },
+  {
+      "id": 4,
+      "name": "Felipa Fratczak",
+      "gender": "Female",
+      "country": "France",
+      "phoneNumber": "+33 989 716 5735",
+      "favorites": [
+          "food",
+          "game",
+          "film"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/ff4444/ffffff"
+  },
+  {
+      "id": 5,
+      "name": "Vonnie Gaythor",
+      "gender": "Male",
+      "country": "Cuba",
+      "phoneNumber": "+53 476 157 8352",
+      "favorites": [
+          "game",
+          "drink",
+          "food",
+          "sport"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/ff4444/ffffff"
+  },
+  {
+      "id": 6,
+      "name": "Farly Platfoot",
+      "gender": "Male",
+      "country": "United States",
+      "phoneNumber": "+1 904 667 2589",
+      "favorites": [
+          "music",
+          "sport",
+          "drink",
+          "food"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/5fa2dd/ffffff"
+  },
+  {
+      "id": 7,
+      "name": "Iain Arboine",
+      "gender": "Male",
+      "country": "France",
+      "phoneNumber": "+33 846 386 2636",
+      "favorites": [
+          "food",
+          "game",
+          "film",
+          "music",
+          "drink"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/cc0000/ffffff"
+  },
+  {
+      "id": 8,
+      "name": "Tori Skelbeck",
+      "gender": "Female",
+      "country": "United States",
+      "phoneNumber": "+1 330 718 7213",
+      "favorites": [
+          "game",
+          "film",
+          "food"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/cc0000/ffffff"
+  },
+  {
+      "id": 9,
+      "name": "Edin Handasyde",
+      "gender": "Male",
+      "country": "France",
+      "phoneNumber": "+33 414 410 0446",
+      "favorites": [
+          "food",
+          "drink",
+          "sport"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/5fa2dd/ffffff"
+  },
+  {
+      "id": 10,
+      "name": "Hanson Beccles",
+      "gender": "Female",
+      "country": "France",
+      "phoneNumber": "+33 720 487 0458",
+      "favorites": [
+          "game",
+          "sport"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/ff4444/ffffff"
+  },
+  {
+      "id": 11,
+      "name": "Emlyn Lawday",
+      "gender": "Male",
+      "country": "France",
+      "phoneNumber": "+33 630 940 3672",
+      "favorites": [
+          "game",
+          "music",
+          "film",
+          "food",
+          "drink"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/cc0000/ffffff"
+  },
+  {
+      "id": 12,
+      "name": "Elna Fussen",
+      "gender": "Female",
+      "country": "United States",
+      "phoneNumber": "+1 972 370 0068",
+      "favorites": [
+          "music",
+          "film",
+          "food",
+          "drink",
+          "sport"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/dddddd/000000"
+  },
+  {
+      "id": 13,
+      "name": "Alicea Kob",
+      "gender": "Male",
+      "country": "France",
+      "phoneNumber": "+33 943 416 5986",
+      "favorites": [
+          "drink",
+          "food",
+          "sport"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/dddddd/000000"
+  },
+  {
+      "id": 14,
+      "name": "Britt Moland",
+      "gender": "Female",
+      "country": "United States",
+      "phoneNumber": "+1 919 602 9409",
+      "favorites": [
+          "food",
+          "sport",
+          "music"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/dddddd/000000"
+  },
+  {
+      "id": 15,
+      "name": "Byrle Spittles",
+      "gender": "Female",
+      "country": "United States",
+      "phoneNumber": "+1 806 275 1725",
+      "favorites": [
+          "film",
+          "music",
+          "sport"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/5fa2dd/ffffff"
+  },
+  {
+      "id": 16,
+      "name": "Cyndy Spreadbury",
+      "gender": "Female",
+      "country": "France",
+      "phoneNumber": "+33 522 715 4122",
+      "favorites": [
+          "music",
+          "film",
+          "drink",
+          "game",
+          "sport"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/ff4444/ffffff"
+  },
+  {
+      "id": 17,
+      "name": "Kennett Waycott",
+      "gender": "Female",
+      "country": "France",
+      "phoneNumber": "+33 395 470 4391",
+      "favorites": [
+          "drink",
+          "food",
+          "music"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/cc0000/ffffff"
+  },
+  {
+      "id": 18,
+      "name": "Giuseppe Kausche",
+      "gender": "Female",
+      "country": "France",
+      "phoneNumber": "+33 515 493 0966",
+      "favorites": [
+          "drink",
+          "film",
+          "game"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/dddddd/000000"
+  },
+  {
+      "id": 19,
+      "name": "Raimund Scottini",
+      "gender": "Male",
+      "country": "Vietnam",
+      "phoneNumber": "+84 244 357 0824",
+      "favorites": [
+          "game",
+          "film",
+          "drink",
+          "sport",
+          "music"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/dddddd/000000"
+  },
+  {
+      "id": 20,
+      "name": "Cinderella Ansell",
+      "gender": "Female",
+      "country": "United States",
+      "phoneNumber": "+1 619 657 8975",
+      "favorites": [
+          "food",
+          "film"
+      ],
+      "avatar": "http://dummyimage.com/200x300.png/dddddd/000000"
+  }
+]
+
+
+function findUserByName() {
+    let j=0;
+  let inputName = document.getElementById("name").value
+      for (let i = 0; i < data.length; i++) {
+          if(data[i].name.toLowerCase()==inputName.toLowerCase())  {
+              console.log(data[i]);
+              j=1;
+          }          
+      }
+      if(j==0) console.log("Doesn't exist");
+      
+    }
+
